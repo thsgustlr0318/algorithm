f1bbe772e9216d2842ffc32d89953752e8e9edf9
thsgustlr0318 <61684785+thsgustlr0318@users.noreply.github.com>
2020-03-07 00:42:57 +0900

    commit

---
+++ 백준/solved.ac-class4/14938_서강그라운드_G3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define INF 1e9
+int item[101], dist[101][101];
+int main()
+{
+	int n, m, r, a, b, l, ans = 0;
+	cin >> n >> m >> r;
+	for (int i = 1; i <= n; i++) {
+		cin >> item[i];
+		for (int j = 1; j <= n; j++) {
+			dist[i][j] = INF;
+			if (i == j) dist[i][j] = 0;
+		}
+	}
+	for (int i = 1; i <= r; i++) {
+		cin >> a >> b >> l;
+		dist[a][b] = dist[b][a] = l;
+	}
+	for (int k = 1; k <= n; k++)
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+	for (int i = 1; i <= n; i++) {
+		int temp = 0;
+		for (int j = 1; j <= n; j++)
+			if (dist[i][j] <= m) temp += item[j];
+		ans = max(ans, temp);
+	}
+	cout << ans;
+}
+//플로이드-와살
+//수색범위(m)보다 낮으면 더함
---
+++ 백준/solved.ac-class4/1786_찾기_G2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int dp[1000001];
+int main()
+{
+	string t, p;
+	vector<int> v;
+	getline(cin, t);
+	getline(cin, p);
+	int psize = p.size(), tsize = t.size();
+	for (int index = 1, compare = 0; index < psize; index++) {
+		while (compare > 0 && p[index] != p[compare]) {
+			compare = dp[compare - 1];
+		}
+		if (p[index] == p[compare])
+			dp[index] = ++compare;
+	}	
+	for (int t_index = 0, p_index = 0; t_index < tsize; t_index++) {
+		if (t[t_index] == p[p_index]) {
+			p_index++;
+			if (p_index == psize) {
+				v.push_back(t_index - psize + 2);
+				p_index = dp[p_index - 1];
+			}
+		}
+		else {
+			if (t[t_index] == p[dp[p_index - 1]])
+				p_index = dp[p_index - 1] + 1;
+			else
+				p_index = 0;
+		}
+	}	
+	cout << v.size() << "\n";
+	for (auto& element : v)
+		cout << element << " ";
+}
+//KMP알고리즘
+//중간부분 
+/*
+for (int index = 0, compare = 0; index < tsize; index++) {
+	while (compare > 0 && t[index] != p[compare]) {
+		compare = dp[compare - 1];
+	}
+	if (t[index] == p[compare]) {
+		compare++;
+		if (compare == psize) {
+			v.push_back(index - psize + 2);
+			compare = dp[compare - 1];
+		}
+	}
+}
+*/
