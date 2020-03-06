11b5ae63ba6406a3d566e65f1699a60313f5b7cd
thsgustlr0318 <61684785+thsgustlr0318@users.noreply.github.com>
2020-03-07 00:54:01 +0900

    commit

--- 백준/solved.ac-class4/1786_찾기_G2.cpp
+++ 백준/solved.ac-class4/1786_찾기_G2.cpp
@@ -36,7 +36,7 @@ int main()
 	for (auto& element : v)
 		cout << element << " ";
 }
-//KMP 알고리즘
+//KMP
 /*
 for (int index = 0, compare = 0; index < tsize; index++) {
 	while (compare > 0 && t[index] != p[compare]) {
